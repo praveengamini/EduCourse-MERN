c82ed33149e177acd4642541bb4af9dbb653c8c3
sathishsara1 <sathishsara1007@gmail.com>
2025-07-20 10:22:51 +0530

    Resolved Conflicts when merging branch FEAT-AddingCourse,EnrollingStudent

--- server/models/CourseVideoModel.js
+++ server/models/CourseVideoModel.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { Schema,model } from "mongoose";
-const CourseVideo = Schema({
-    title : {type:String,required:true},
-    url : {type:String,required:true},
-    duration : {type:String,required:true},
-})
-const CourseVideoModel = model('CourseVideoModel',CourseVideo);
-export default CourseVideoModel;
-=======
 // models/CourseVideoModel.js
 
 const mongoose = require('mongoose');
@@ -28,4 +18,3 @@ const courseVideoSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('CourseVideo', courseVideoSchema);
->>>>>>> FEAT-AddingCourse,EnrollingStudent

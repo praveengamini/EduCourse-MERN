afd3720b67285d26c4dfbe90f1620642997cd8a7
SAM <saisampath453@gmail.com>
2025-07-26 18:18:11 +0530

    limited video access to unsubscriped users 26/07/2025 18:18

--- server/routes/student/student-routes.js
+++ server/routes/student/student-routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { sendEmail } = require("../../controllers/student/sendMail.js");
-
+const {courseToStudent} = require("../../controllers/student/CourseToStudent.js");
 const router = express.Router();
 router.post("/sendEmail", sendEmail);
+router.get('/courseToStudent',courseToStudent);
 
 module.exports = router;

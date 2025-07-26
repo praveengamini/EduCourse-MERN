5edd74534b51b001c08f09691d1b0fd7cb1ab295
SAM <saisampath453@gmail.com>
2025-07-26 10:51:55 +0530

    email module created 26/07/2025 10:51

---
+++ server/controllers/student/sendMail.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (req, res) => {
+  console.log(req.body);
+  
+  const { name, email, contactNumber, courseName,userID,description } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+     tls: {
+    rejectUnauthorized: false  
+  }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: "New Form Submission",
+    text: `From: ${name}\nEmail: ${email}\nUser ID : ${userID}\nContact Number: ${contactNumber}\nCourse Name: ${courseName}\ndescription :${description}`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ messageSent: true });
+  } catch (err) {
+    console.error("Error sending email:", err);
+    res.status(500).json({ messageSent: false});
+  }
+};
+
+module.exports = { sendEmail };

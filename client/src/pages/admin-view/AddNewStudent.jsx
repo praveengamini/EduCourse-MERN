4402e0f89f089afd3c418533d6a146c558f9f4a7
praveen <praveengamini009@gmail.com>
2025-08-28 23:47:00 +0530

    created add Add student

---
+++ client/src/pages/admin-view/AddNewStudent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { toast } from 'sonner'
+const AddNewStudent = () => {
+  return (
+    <div>
+      Adding new student
+    </div>
+  )
+}
+
+export default AddNewStudent

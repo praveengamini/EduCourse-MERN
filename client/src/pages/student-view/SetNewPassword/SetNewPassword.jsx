b2a7d2e344838bc2bd515bfea5ddea7054ca69fc
praveen <praveengamini009@gmail.com>
2025-08-30 20:52:06 +0530

    set new password initiated

---
+++ client/src/pages/student-view/SetNewPassword/SetNewPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SetNewPassword = () => {
+  return (
+    <div>
+      this page for setting the new password
+    </div>
+  )
+}
+
+export default SetNewPassword

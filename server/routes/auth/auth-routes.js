e04651a66bc1309f0c844952b5c7c5f2c83187e7
praveen <praveengamini009@gmail.com>
2025-08-31 00:17:07 +0530

    register route removed

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const Router = express.Router;
-const { registerUser, loginUser, logoutUser, authMiddleware,setNewPassword } = require("../../controllers/auth/auth-controller.js");
+const {loginUser, logoutUser, authMiddleware,setNewPassword } = require("../../controllers/auth/auth-controller.js");
 
 const router = Router();
 
-router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", authMiddleware, logoutUser);
 router.put('/set-password', authMiddleware, setNewPassword);
